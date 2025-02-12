7a9310bbb605c3e04536e67b9789fb5cb119d47f
zhengke <zhengke996@163.com>
2025-02-12 21:57:12 +0800

    build(deps): 添加 @types/node 依赖并配置路径别名
    - 在 devDependencies 中添加 @types/node^22.13.1
    - 在 vite.config.ts 中添加路径别名配置，将 "@" 指向 "src" 目录
    - 更新 pnpm-lock.yaml 中的相关依赖版本

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"), // 路径别名
+    },
+  },
 });
